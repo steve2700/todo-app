7d02a2ee8b93a419bdd0e934040b96976cb8c319
steve2700 <stevnyaruwa@gmail.com>
2023-05-23 00:53:14 +0200

    trying

--- script.js
+++ script.js
@@ -16,32 +16,21 @@ form.addEventListener("submit", (e) => {
     addTodo();
 });
 
-function addTodo(todo) {
+function addTodo() {
     let todoText = input.value;
-
-    if (todo) {
-        todoText = todo.text;
-    }
 
     if (todoText) {
         const todoEl = document.createElement("li");
-        if (todo && todo.completed) {
-            todoEl.classList.add("completed");
-        }
-
         todoEl.innerText = todoText;
 
         todoEl.addEventListener("click", () => {
             todoEl.classList.toggle("completed");
-
             updateLS();
         });
 
         todoEl.addEventListener("contextmenu", (e) => {
             e.preventDefault();
-
             todoEl.remove();
-
             updateLS();
         });
 
@@ -55,7 +44,6 @@ function addTodo(todo) {
 
 function updateLS() {
     const todosEl = document.querySelectorAll("li");
-
     const todos = [];
 
     todosEl.forEach((todoEl) => {
@@ -66,15 +54,8 @@ function updateLS() {
     });
 
     localStorage.setItem("todos", JSON.stringify(todos));
-
 }
-function addTodo() {
-    // Retrieve the values of priority and due date
-    let priority = document.getElementById("priorityInput").value;
-    let dueDate = document.getElementById("dueDateInput").value;
 
-    // Rest of the code...
-}
 const clearButton = document.getElementById("clearButton");
 clearButton.addEventListener("click", clearLocalStorage);
 
@@ -82,157 +63,33 @@ function clearLocalStorage() {
     localStorage.removeItem("todos");
     todosUL.innerHTML = "";
 }
+
 const filterSelect = document.getElementById("filterSelect");
-
 filterSelect.addEventListener("change", filterTodos);
+
 function filterTodos() {
-  const selectedValue = filterSelect.value;
-  const todos = document.querySelectorAll("li");
+    const selectedValue = filterSelect.value;
+    const todos = document.querySelectorAll("li");
 
-  todos.forEach((todo) => {
-    if (selectedValue === "all") {
-      // Show all todos
-      todo.style.display = "block";
-    } else if (selectedValue === "completed") {
-      // Show only completed todos
-      if (todo.classList.contains("completed")) {
-        todo.style.display = "block";
-      } else {
-        todo.style.display = "none";
-      }
-    } else if (selectedValue === "not-completed") {
-      // Show only not completed todos
-      if (!todo.classList.contains("completed")) {
-        todo.style.display = "block";
-      } else {
-        todo.style.display = "none";
-      }
-    }
-  });
-}
-// After adding or removing todos
-addTodo();
-// Or
-updateLS();
-// Call filterTodos to update the displayed todos
-filterTodos();
-const todosUL = document.getElementById("todos");
-
-// Add event listener to the todos list
-todosUL.addEventListener("dblclick", handleTodoEdit);
-
-function handleTodoEdit(event) {
-  const todoItem = event.target.closest("li");
-  const todoTextElement = todoItem.querySelector(".todo-text");
-  
-  // Enable editing mode by adding an input field
-  const inputElement = document.createElement("input");
-  inputElement.type = "text";
-  inputElement.value = todoTextElement.textContent;
-  
-  inputElement.addEventListener("keydown", function (event) {
-    if (event.key === "Enter") {
-      finishEditingTodoItem(todoItem, inputElement);
-    }
-  });
-  
-  // Replace the todo text element with the input field
-  todoItem.replaceChild(inputElement, todoTextElement);
-  
-  inputElement.focus();
-}
-
-function finishEditingTodoItem(todoItem, inputElement) {
-  const newTodoText = inputElement.value;
-  
-  // Create a new span element to replace the input field
-  const todoTextElement = document.createElement("span");
-  todoTextElement.classList.add("todo-text");
-  todoTextElement.textContent = newTodoText;
-  
-  // Replace the input field with the todo text element
-  todoItem.replaceChild(todoTextElement, inputElement);
-  
-  updateTodoTextInLocalStorage(todoItem, newTodoText);
-}
-
-function updateTodoTextInLocalStorage(todoItem, newText) {
-  const todos = JSON.parse(localStorage.getItem("todos"));
-  
-  const todoIndex = Array.from(todosUL.children).indexOf(todoItem);
-  if (todoIndex !== -1) {
-    todos[todoIndex].text = newText;
-    localStorage.setItem("todos", JSON.stringify(todos));
-  }
-}
-todosUL.addEventListener("contextmenu", (e) => {
-    e.preventDefault();
-
-    const todoItem = e.target.closest("li");
-
-    if (todoItem) {
-        const todoText = todoItem.querySelector(".todo-text").textContent;
-        const confirmDelete = confirm(`Are you sure you want to delete "${todoText}"?`);
-
-        if (confirmDelete) {
-            todoItem.remove();
-            updateLS();
+    todos.forEach((todo) => {
+        if (selectedValue === "all") {
+            // Show all todos
+            todo.style.display = "block";
+        } else if (selectedValue === "completed") {
+            // Show only completed todos
+            if (todo.classList.contains("completed")) {
+                todo.style.display = "block";
+            } else {
+                todo.style.display = "none";
+            }
+        } else if (selectedValue === "not-completed") {
+            // Show only not completed todos
+            if (!todo.classList.contains("completed")) {
+                todo.style.display = "block";
+            } else {
+                todo.style.display = "none";
+            }
         }
-    }
-});
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
-
-    addTodo();
-});
-function updateLocalStorage() {
-    const todosEl = document.querySelectorAll("li");
-
-    const todos = Array.from(todosEl).map(todoEl => {
-        return {
-            text: todoEl.innerText,
-            completed: todoEl.classList.contains("completed")
-        };
-    });
-
-    localStorage.setItem("todos", JSON.stringify(todos));
-}
-const todos = JSON.parse(localStorage.getItem("todos"));
-
-if (todos) {
-    todos.forEach(todo => {
-        addTodo(todo);
     });
 }
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
-
-    addTodo();
-});
-function addTodo(todo) {
-    // ...
-
-    if (todoText) {
-        // ...
-
-        todoEl.addEventListener("click", () => {
-            todoEl.classList.toggle("completed");
-
-            updateLocalStorage();
-        });
-
-        todoEl.addEventListener("contextmenu", (e) => {
-            e.preventDefault();
-
-            todoEl.remove();
-
-            updateLocalStorage();
-        });
-
-        // ...
-
-        updateLocalStorage();
-    }
-}
-
 
