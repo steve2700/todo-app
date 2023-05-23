12de09dfd395c13d2bf18b598e6b8f846b931995
steve2700 <stevnyaruwa@gmail.com>
2023-05-23 20:37:21 +0200

    it did happen

--- script.js
+++ script.js
@@ -1,170 +1,151 @@
-let todos = [];
-
 const form = document.getElementById("form");
 const input = document.getElementById("input");
 const priorityInput = document.getElementById("priorityInput");
 const dueDateInput = document.getElementById("dueDateInput");
-const todosList = document.getElementById("todos");
+const addButton = document.getElementById("addButton");
+const todos = document.getElementById("todos");
 const filterSelect = document.getElementById("filterSelect");
 const clearButton = document.getElementById("clearButton");
+const profilePictureInput = document.getElementById("profilePictureInput");
+const usernameInput = document.getElementById("usernameInput");
+const saveProfileButton = document.getElementById("saveProfileButton");
 const profilePicture = document.getElementById("profilePicture");
 const username = document.getElementById("username");
 
-// Load profile and todos from local storage
-loadData();
+let todoList = [];
 
-// Render profile and todos
-renderProfile();
-renderTodos();
+function addTodo(event) {
+    event.preventDefault();
 
-// Event listener for form submission
-form.addEventListener("submit", function (event) {
-  event.preventDefault();
-  addTodo();
-});
+    const todoText = input.value.trim();
+    const priority = priorityInput.value.trim();
+    const dueDate = dueDateInput.value.trim();
 
-// Event listener for filter select
-filterSelect.addEventListener("change", function () {
-  renderTodos();
-});
+    if (todoText === "") {
+        return;
+    }
 
-// Event listener for clear button
-clearButton.addEventListener("click", function () {
-  clearTodos();
-});
-
-// Event listener for profile picture edit
-const editProfileButton = document.getElementById("editProfileButton");
-editProfileButton.addEventListener("click", function () {
-  const newProfilePicture = prompt("Enter the URL of your new profile picture:");
-  if (newProfilePicture) {
-    updateProfilePicture(newProfilePicture);
-  }
-});
-
-// Function to add a new todo
-function addTodo() {
-  const todoText = input.value.trim();
-  const priority = priorityInput.value.trim();
-  const dueDate = dueDateInput.value;
-
-  if (todoText !== "") {
     const todo = {
-      id: new Date().getTime(),
-      text: todoText,
-      priority: priority,
-      dueDate: dueDate,
-      completed: false,
-   };
+        id: Date.now(),
+        text: todoText,
+        priority: priority,
+        dueDate: dueDate,
+        completed: false,
+    };
 
-    todos.push(todo);
-    saveData();
-    renderTodos();
+    todoList.push(todo);
+    saveTodoList();
 
     input.value = "";
     priorityInput.value = "";
     dueDateInput.value = "";
-    input.focus();
-  }
+
+    renderTodoList();
 }
 
-// Function to render profile
-function renderProfile() {
-  const storedProfilePicture = localStorage.getItem("profilePicture");
-  const storedUsername = localStorage.getItem("username");
-
-  if (storedProfilePicture) {
-    profilePicture.style.backgroundImage = `url('${storedProfilePicture}')`;
-  }
-
-  if (storedUsername) {
-    username.textContent = storedUsername;
-  }
-}
-
-// Function to update profile picture
-function updateProfilePicture(newProfilePicture) {
-  profilePicture.style.backgroundImage = `url('${newProfilePicture}')`;
-  localStorage.setItem("profilePicture", newProfilePicture);
-}
-
-// Function to save todos to local storage
-function saveData() {
-  localStorage.setItem("todos", JSON.stringify(todos));
-}
-
-// Function to load todos from local storage
-function loadData() {
-  const storedTodos = localStorage.getItem("todos");
-  if (storedTodos) {
-    todos = JSON.parse(storedTodos);
-  }
-}
-
-// Function to render todos
-function renderTodos() {
-  todosList.innerHTML = "";
-
-  const filteredTodos = filterSelect.value === "all"
-    ? todos
-    : filterSelect.value === "completed"
-      ? todos.filter(todo => todo.completed)
-      : todos.filter(todo => !todo.completed);
-
-  filteredTodos.forEach(todo => {
-    const todoElement = document.createElement("li");
-    todoElement.classList.add("todo");
-    if (todo.completed) {
-      todoElement.classList.add("complete");
+function toggleComplete(todoId) {
+    const todo = todoList.find((todo) => todo.id === todoId);
+    if (todo) {
+        todo.completed = !todo.completed;
+        saveTodoList();
+        renderTodoList();
     }
-
-    todoElement.innerHTML = `
-      <span>${todo.text} - Priority: ${todo.priority} - Due Date: ${todo.dueDate}</span>
-      <div class="todo-actions">
-        <button class="delete-button" onclick="deleteTodo(${todo.id})">&#10006;</button>
-      </div>
-    `;
-
-    todoElement.addEventListener("click", function () {
-      toggleTodoComplete(todo.id);
-    });
-
-    todoElement.addEventListener("contextmenu", function (event) {
-      event.preventDefault();
-      deleteTodo(todo.id);
-    });
-
-    todosList.appendChild(todoElement);
-  });
 }
 
-// Function to toggle todo completion status
-function toggleTodoComplete(todoId) {
-  todos = todos.map(todo => {
-    if (todo.id === todoId) {
-      return {
-        ...todo,
-        completed: !todo.completed
-      };
-    }
-    return todo;
-  });
-
-  saveData();
-  renderTodos();
-}
-
-// Function to delete a todo
 function deleteTodo(todoId) {
-  todos = todos.filter(todo => todo.id !== todoId);
-  saveData();
-  renderTodos();
+    const todoIndex = todoList.findIndex((todo) => todo.id === todoId);
+    if (todoIndex > -1) {
+        todoList.splice(todoIndex, 1);
+        saveTodoList();
+        renderTodoList();
+    }
 }
 
-// Function to clear all todos
-function clearTodos() {
-  todos = [];
-  saveData();
-  renderTodos();
+function clearAll() {
+    todoList = [];
+    saveTodoList();
+    renderTodoList();
 }
+
+function saveTodoList() {
+    localStorage.setItem("todoList", JSON.stringify(todoList));
+}
+
+function loadTodoList() {
+    const storedTodoList = localStorage.getItem("todoList");
+    if (storedTodoList) {
+        todoList = JSON.parse(storedTodoList);
+        renderTodoList();
+    }
+}
+
+function renderTodoList() {
+    todos.innerHTML = "";
+
+    const filteredTodos = filterTodos();
+
+    filteredTodos.forEach((todo) => {
+        const todoItem = document.createElement("li");
+        todoItem.classList.add("todo-item");
+        if (todo.completed) {
+            todoItem.classList.add("completed");
+        }
+
+        const todoText = document.createElement("span");
+        todoText.classList.add("todo-text");
+        todoText.innerText = todo.text;
+        todoText.addEventListener("click", () => toggleComplete(todo.id));
+
+        const todoActions = document.createElement("div");
+        todoActions.classList.add("todo-actions");
+
+        const deleteButton = document.createElement("button");
+        deleteButton.innerHTML = '<i class="fas fa-trash"></i>';
+        deleteButton.addEventListener("click", (event) => {
+            event.stopPropagation();
+            deleteTodo(todo.id);
+        });
+
+        todoActions.appendChild(deleteButton);
+
+        todoItem.appendChild(todoText);
+        todoItem.appendChild(todoActions);
+
+        todos.appendChild(todoItem);
+    });
+}
+
+function filterTodos() {
+    const filterValue = filterSelect.value;
+    if (filterValue === "completed") {
+        return todoList.filter((todo) => todo.completed);
+    } else if (filterValue === "not-completed") {
+        return todoList.filter((todo) => !todo.completed);
+    } else {
+        return todoList;
+    }
+}
+
+function saveProfile(event) {
+    event.preventDefault();
+
+    const profilePictureFile = profilePictureInput.files[0];
+    const usernameText = usernameInput.value.trim();
+
+    if (profilePictureFile && usernameText !== "") {
+        const reader = new FileReader();
+        reader.onload = () => {
+            profilePicture.style.backgroundImage = `url('${reader.result}')`;
+        };
+        reader.readAsDataURL(profilePictureFile);
+
+        username.innerText = usernameText;
+    }
+}
+
+form.addEventListener("submit", addTodo);
+clearButton.addEventListener("click", clearAll);
+saveProfileButton.addEventListener("click", saveProfile);
+
+loadTodoList();
 
